8b59362f023f00f6a6c5334acb4f21cc84129915
yuqi-wang-forever <1915780288@qq.com>
2024-02-01 12:47:00 +0800

    结构修改

---
+++ src/main/java/wang/yuqi/springexampleyuqi/SpringExampleYuqiApplication.java
@@ -0,0 +1,13 @@
+package wang.yuqi.springexampleyuqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExampleYuqiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExampleYuqiApplication.class, args);
+    }
+
+}
